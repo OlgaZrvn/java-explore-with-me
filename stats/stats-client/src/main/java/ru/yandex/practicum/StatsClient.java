e0cfb9c07bf21c3ab574d67df5c5430db38dfc73
Ольга <olga.zrvn@yandex.ru>
2024-05-03 13:07:53 +0500

    Часть 1

--- stats/stats-client/src/main/java/ru/yandex/practicum/StatsClient.java
+++ stats/stats-client/src/main/java/ru/yandex/practicum/StatsClient.java
@@ -1,31 +1,87 @@
 package ru.yandex.practicum;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import io.micrometer.core.lang.Nullable;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Service
-public class StatsClient extends BaseClient {
+public class StatsClient {
+    private final RestTemplate rest;
 
-    private static final String API_PREFIX = "/stats";
-
-    @Autowired
-    public StatsClient(@Value("${stats-client.url}") String serverUrl, RestTemplateBuilder builder) {
-        super(
-                builder
-                        .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + API_PREFIX))
-                        .requestFactory(HttpComponentsClientHttpRequestFactory::new)
-                        .build()
-        );
+    public StatsClient(@Value("http://localhost:9090") String statsServerUrl, RestTemplateBuilder builder) {
+        this.rest = builder
+                .uriTemplateHandler(new DefaultUriBuilderFactory(statsServerUrl))
+                .requestFactory(HttpComponentsClientHttpRequestFactory::new)
+                .build();
     }
 
-    public ResponseEntity<Object> createHit(EndpointHit endpointHit) {
-        return post("", endpointHit);
+    public ResponseEntity<Object> addHit(EndpointHit endpointHit) {
+        return makeAndSendRequest(HttpMethod.POST, "/hit", null, endpointHit);
     }
 
+    public ResponseEntity<Object> getViewStats(LocalDateTime start,
+                                               LocalDateTime end,
+                                               List<String> uris,
+                                               Boolean unique) {
+        Map<String, Object> parameters = new HashMap<>(Map.of(
+                "start", start,
+                "end", end,
+                "unique", unique
+        ));
+        String path;
+        if (uris == null) {
+            path = "/stats?start={start}&end={end}&unique={unique}";
+        } else {
+            parameters.put("uris", uris);
+            path = "/stats?start={start}&end={end}&uris={uris}&unique={unique}";
+        }
+        return makeAndSendRequest(HttpMethod.GET, path, parameters, null);
+    }
+
+    private <T> ResponseEntity<Object> makeAndSendRequest(HttpMethod method,
+                                                          String path,
+                                                          @Nullable Map<String, Object> parameters,
+                                                          @Nullable T body) {
+        HttpEntity<T> requestEntity = new HttpEntity<>(body, defaultHeaders());
+        ResponseEntity<Object> statsServerResponse;
+        try {
+            if (parameters != null) {
+                statsServerResponse = rest.exchange(path, method, requestEntity, Object.class, parameters);
+            } else {
+                statsServerResponse = rest.exchange(path, method, requestEntity, Object.class);
+            }
+        } catch (HttpStatusCodeException e) {
+            return ResponseEntity.status(e.getStatusCode()).body(e.getResponseBodyAsByteArray());
+        }
+        return prepareGatewayResponse(statsServerResponse);
+    }
+
+    private HttpHeaders defaultHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        return headers;
+    }
+
+    private static ResponseEntity<Object> prepareGatewayResponse(ResponseEntity<Object> response) {
+        if (response.getStatusCode().is2xxSuccessful()) {
+            return response;
+        }
+        ResponseEntity.BodyBuilder responseBuilder = ResponseEntity.status(response.getStatusCode());
+        if (response.hasBody()) {
+            return responseBuilder.body(response.getBody());
+        }
+        return responseBuilder.build();
+    }
 }
-
